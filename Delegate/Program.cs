89e953b04f3c9773140bab85402b236864ab5603
kjwaxon <kaanaslan1996@hotmail.com>
2022-04-19 14:18:57 +0300

    Delegate
    Dikdörtgen alan ve çevre

---
+++ Delegate/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Delegate
+{
+    class Program
+    {
+        /*
+         *Delegateler bir nevi metod pointerlarıdır.
+         *Yani metodları yönetirler
+         *Delegate tipinden bir nesne türetip rahatlıkla işlemler yapmamızı sağlarlar.
+         * 
+         * Aşağıdaki örnekte çevre ve alan hesabı yapan metodlarımın ana metodum içerisinde delegateden nesne türetip parametre geçirerek kullandım.
+         * Ayrıca Delegate ın  parametresi içindeki değişkenlerinde aşağıdakilerle aynı olma zorunluluğu yoktur.
+         */
+
+        delegate double Dikdörtgen(double x, double y);
+        static void Main(string[] args)
+        {
+            Dikdörtgen test = new Dikdörtgen(alan);
+            Console.WriteLine(test(5,7));
+           
+            Dikdörtgen test2 = new Dikdörtgen(cevre);
+            Console.WriteLine(test2(10,25));
+            Console.ReadKey();
+        }
+        static double cevre(double a, double b)
+        {
+            return 2 * (a + b);
+        }
+        static double alan(double a , double b)
+        {
+            return a * b;
+        }
+    }
+}
